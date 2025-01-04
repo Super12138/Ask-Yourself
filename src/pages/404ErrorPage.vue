f9488f8b3107e793d96aba7375c50de6e50443c2
Super12138 <70494801+Super12138@users.noreply.github.com>
2025-01-04 11:18:33 +0800

    feat: 404页面的样式

--- src/pages/404ErrorPage.vue
+++ src/pages/404ErrorPage.vue
@@ -1,8 +1,35 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import CenterItem from '@/components/CenterItem.vue';
+
+import 'mdui/components/button.js';
+
+import '@mdui/icons/report-gmailerrorred--outlined.js';
 </script>
 
 <template>
-    <div>
-        <h1>找不到页面了了了了了了了了</h1>
-    </div>
+    <CenterItem>
+        <template #icon>
+            <mdui-icon-report-gmailerrorred--outlined></mdui-icon-report-gmailerrorred--outlined>
+        </template>
+        <template #content>
+            <h2>未找到页面</h2>
+            <p>别担心，点击下方按钮回到题库页面吧</p>
+            <RouterLink to="/" custom v-slot="{ navigate }">
+                <mdui-button variant="tonal" @click="navigate">回到题库</mdui-button>
+            </RouterLink>
+        </template>
+    </CenterItem>
 </template>
+
+<style lang="css" scoped>
+mdui-icon-report-gmailerrorred--outlined {
+    height: 3rem;
+    width: 3rem;
+    margin: 1rem;
+}
+
+mdui-button {
+    margin: 0.5rem;
+}
+</style>
