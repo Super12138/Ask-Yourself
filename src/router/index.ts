77e2ecc3446f304ed5b8820d81afcc86e1ba558a
Super12138 <70494801+Super12138@users.noreply.github.com>
2025-10-04 20:49:09 +0800

    refactor: 路由使用 Web Hash 模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import NotFoundPage from "@/pages/NotFoundPage.vue";
-import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
     { path: "/", component: () => import("@/pages/main/MainPage.vue") },
@@ -8,7 +8,7 @@ const routes: RouteRecordRaw[] = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: routes,
 });
 
